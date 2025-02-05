db81c06c6d9d30beec328bfbc2bb689aa69d2a56
Shubham Gupta <shubhamedu.01@gmail.com>
2025-02-05 16:21:18 +0530

    build fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,26 +1,26 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { GoogleAnalytics } from "@next/third-parties/google";
-export const metadata = {
+export const metadata: Metadata = {
   title: "Shubham Gupta – Senior Software Engineer at Credilio",
   description:
-    "Shubham Gupta is a Senior Software Engineer at Credilio, specializing in frontend development with expertise in React, Next.js, and TypeScript.",
+    "Shubham Gupta is a Senior Software Engineer at Credilio, specializing in frontend development with expertise in React, Next.js, vue, and TypeScript.",
   keywords:
-    "Shubham Gupta, Senior Software Engineer, Credilio, Frontend Developer, React, Next.js, TypeScript, JavaScript, Web Development, Software Engineer, Full Stack Developer",
+    "Shubham Gupta, Senior Software Engineer, Credilio, Frontend Developer, React, Next.js, vue, TypeScript, JavaScript, Web Development, Software Engineer, Full Stack Developer",
   openGraph: {
     title: "Shubham Gupta – Senior Software Engineer at Credilio",
     description:
-      "Shubham Gupta is a Senior Software Engineer at Credilio, specializing in frontend development with expertise in React, Next.js, and TypeScript.",
+      "Shubham Gupta is a Senior Software Engineer at Credilio, specializing in frontend development with expertise in React, Next.js, vue, and TypeScript.",
     url: "https://www.shubhamgupta.dev",
-    image: "https://www.shubhamgupta.dev/images/profile.jepg",
+    images: "https://www.shubhamgupta.dev/images/profile.jepg",
     type: "website",
   },
   twitter: {
     card: "summary_large_image",
     title: "Shubham Gupta – Senior Software Engineer at Credilio",
     description:
-      "Shubham Gupta is a Senior Software Engineer at Credilio, specializing in frontend development with expertise in React, Next.js, and TypeScript.",
-    image: "https://www.shubhamgupta.dev/images/profile.jepg",
+      "Shubham Gupta is a Senior Software Engineer at Credilio, specializing in frontend development with expertise in React, Next.js, vue, and TypeScript.",
+    images: "https://www.shubhamgupta.dev/images/profile.jepg",
   },
 };
 
